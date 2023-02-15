d505ea90b710589d1b431d32fcb4c15d92f3359a
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-15 12:24:01 -0500

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    else{
 
+    
+    let finalString = "";
+    for (let i =0; i<num; i++){
+        finalString += string;
+    }
+    return finalString; 
+}
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
