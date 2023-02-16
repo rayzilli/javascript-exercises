b6f76d05f480ccc4303ebcc60abe7703f8f48df2
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-16 10:35:52 -0500

    Works for first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber,endNumber) {
+    if (startNumber > endNumber){
+        
+    }
+    let totalSum = startNumber; 
+    for (let i = startNumber + 1; i <= endNumber; i++){
+     totalSum += i;
+    }
+   return totalSum; 
 };
 
 // Do not edit below this line
