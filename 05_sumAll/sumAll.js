375957bccd0caf03f16f329d92195ad5aac29a75
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-16 10:38:10 -0500

    Removed line that wasn't needed
    solved first two questions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,5 @@
 const sumAll = function(startNumber,endNumber) {
-    if (startNumber > endNumber){
-        
-    }
+
     let totalSum = startNumber; 
     for (let i = startNumber + 1; i <= endNumber; i++){
      totalSum += i;
