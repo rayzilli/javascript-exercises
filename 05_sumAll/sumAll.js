3a081b19c0323f64ae061115293f347a3ea834f1
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-16 11:50:08 -0500

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,31 @@
 const sumAll = function(startNumber,endNumber) {
 
+    if (typeof startNumber === "string" || typeof endNumber === "string"){
+        return "ERROR"
+    }
+    else if (isNaN(startNumber) || isNaN(endNumber)){
+        return "ERROR"
+    }
+    
+    else if(startNumber < 0 || endNumber < 0){
+        return "ERROR"
+    }
+
+    else  if(startNumber > endNumber){
+        let totalSum = endNumber; 
+    for (let i = endNumber + 1; i <= startNumber; i++){
+     totalSum += i;
+    }
+   return totalSum; 
+    }
+
+    else{
     let totalSum = startNumber; 
     for (let i = startNumber + 1; i <= endNumber; i++){
      totalSum += i;
     }
-   return totalSum; 
+       return totalSum; 
+}
 };
 
 // Do not edit below this line
