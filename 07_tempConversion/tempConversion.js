d79b4301af15127e9aafffa3d47999ecda4e5d96
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-02-16 13:09:56 -0500

    Completed exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion  = (temp-32) * 5/9;
+  let roundNumber = Math.round(conversion*10)/10
+  return roundNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * 9/5) + 32;
+  let roundNumber = Math.round(conversion*10)/10
+  return roundNumber;
 };
 
 // Do not edit below this line
