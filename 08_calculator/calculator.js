09ff25b2f72a4e8a02bad21417ac70b84d7cc067
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-03-03 12:15:43 -0500

    completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+	return num1 - num2; 
+};
+
+const sum = function(calculator) {
+   const totalSum = calculator.reduce((a, b) => a + b, 0);
+   return totalSum 
+};
+
+const multiply = function(calculator) {
+  const totalMultiply = calculator.reduce((a,b)=> a * b);
+  return totalMultiply 
+
+};
+
+const power = function(number,exponential) {
+  const powerTotal = number ** exponential;
+  return powerTotal; 
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total = number;
+  if (number ===0){
+    return 1; 
+  }
+  while (number > 1){
+  number --;
+  total = total * number;
+  }
+	return total; 
 };
 
 // Do not edit below this line
