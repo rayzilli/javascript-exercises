39ba807d002728aafbd57cd6377c6c634a33eb6c
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-03-03 14:33:40 -0500

    solved palindromes question

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let regex = /[^A-Z a-z]/g;
+    let removePunctuation = word.replace(regex,"").toLowerCase(); 
+    reverseWord = removePunctuation.split("").reverse().join("").replaceAll(",","").replaceAll(" ","");
+   originalWord = removePunctuation.replaceAll(",","").replaceAll(" ","");
 
+    if (originalWord === reverseWord){
+        return true;
+    }
+    else{
+        return false; 
+    }
 };
 
 // Do not edit below this line
