7555b47d384af61d427d0985f10ac3a46b6d2a0a
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-03-03 23:03:59 -0500

    completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let num1 =0;
+    let num2= 1;
+    let newNumber;
+    if (number <0){
+        return "OOPS"
+    }
+    for (let i=2 ; i<= number; i++){
+        newNumber = num1+num2;
+        num1 = num2;
+        num2 = newNumber;
+    }
+    return newNumber; 
 };
+ console.log(fibonacci(7));
 
 // Do not edit below this line
 module.exports = fibonacci;
