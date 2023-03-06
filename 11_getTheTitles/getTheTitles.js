589c0cae21e3f7ec141b25ce305161cc72796908
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-03-05 23:40:58 -0500

    completed exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+//get array and return just the titles
+const getTheTitles = function(books) {
+  const allBooks =  books.map(books => books.title);
+  return allBooks;
+      
 };
 
 // Do not edit below this line
